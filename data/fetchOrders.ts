189c4664ecfe45b33edae5c14addccc3ce67d464
Wolfgang <sumin@unix-center.ru>
2025-07-10 11:53:36 +0000

    :sparkles: add json bin

--- data/fetchOrders.ts
+++ data/fetchOrders.ts
@@ -3,16 +3,22 @@ type RawOrder = {
   customer: string;
   amount: number;
   createdAt: number;
-}
+};
 
 export async function fetchOrders(): Promise<RawOrder[]> {
   const controller = new AbortController();
   const timeout = setTimeout(() => controller.abort(), 2_000);
 
   try {
-    const res = await fetch('https://example.com/api/v1/orders', {
-      signal: controller.signal,
-    });
+    const res = await fetch(
+      "https://api.jsonbin.io/v3/b/686fa70efb55774d1c72471b",
+      {
+        headers: {
+          "X-Access-Key": "tbd",
+        },
+        signal: controller.signal,
+      }
+    );
 
     return res.json();
   } finally {
